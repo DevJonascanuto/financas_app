b66fb512198df4d48601f27d1145716e4ae748b0
jonas <jonascanuto1@gmail.com>
2026-03-20 20:47:43 -0300

    add guunicorn

--- run.py
+++ run.py
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
-from app import create_app, db
 
-app = create_app()
-
-
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-
-=======
 from financas_app.app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
->>>>>>> 7da8b58 (atualizando o app por completo, reestilizando, e incluindo funçoes de)
     app.run(debug=True)
